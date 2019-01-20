7f31f52bb63da067262fdb73acf54f2d100a6449
Jon Callahan <jon@joncallahan.co>
2019-01-20 14:05:45 -0800

    adds debugger logs

--- src/volkswagen.ts
+++ src/volkswagen.ts
@@ -13,23 +13,28 @@ export interface ITripdata {
 }
 export async function getTripData(): Promise<ITripdata[]> {
   const browser = await puppeteer.launch({ headless: true });
+  console.log('opening new tab');
   const page = await browser.newPage();
+  console.log('Opening carnet login');
   await page.goto('https://carnet.vw.com/web/vwcwp/login');
   await page.keyboard.type(USERNAME);
   await page.keyboard.press('Tab');
   await page.keyboard.type(PASSWORD);
   await page.keyboard.press('Enter');
   await page.waitForNavigation();
+  console.log('entering pin');
   await page.keyboard.press('Tab');
   await page.keyboard.press('Tab');
   await page.keyboard.press('Tab');
   await page.keyboard.type(PIN);
   await page.keyboard.press('Enter');
   await page.waitForNavigation();
+  console.log('going to trip page');
   await page.goto('https://carnet.vw.com/group/vwcwp/trips');
   const dataElement = await page.$(
     '#_tripstatisticsportlet_WAR_tripstatisticsportlet_tripsJson'
   );
+  console.log('found trip data');
   const tripData = JSON.parse(await (await dataElement.getProperty('value')).jsonValue());
   await browser.close();
   console.log(tripData);
