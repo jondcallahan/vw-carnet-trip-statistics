702091cc1e1a9de1ed004c44961d7aaa8ba9a8a0
Jon Callahan <jon@joncallahan.co>
2019-11-12 14:04:23 -0800

    fixes data fetching

--- src/volkswagen.ts
+++ src/volkswagen.ts
@@ -1,6 +1,10 @@
 import * as puppeteer from 'puppeteer';
 require('dotenv').config();
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 const { CARNET_USERNAME, CARNET_PASSWORD, CARNET_PIN } = process.env;
 export interface ITripdata {
   vehicleStatisticsId: string;
@@ -21,25 +25,31 @@ export async function getTripData(): Promise<ITripdata[]> {
   });
   console.log('opening new tab');
   const page = await browser.newPage();
-  console.log('Opening carnet login');
-  await page.goto('https://carnet.vw.com/web/vwcwp/login');
+  console.log('opening carnet login');
+  await page.goto('https://carnet.vw.com/c/portal/login');
+  console.log('entering username');
   await page.keyboard.type(CARNET_USERNAME);
-  await page.keyboard.press('Tab');
+  await page.keyboard.press('Enter');
+  await page.waitForNavigation();
+  console.log('entering password');
   await page.keyboard.type(CARNET_PASSWORD);
   await page.keyboard.press('Enter');
   await page.waitForNavigation();
+  console.log('selecting car from garage');
+  const garageButton = await page.$('a.nav-arrow');
+  await garageButton.click();
+  const pinInput = await page.waitForSelector('input[maxlength="4"]');
+  // The input appears, then turns into a loading indicator, then re-appears
+  // so hackily wait 6 seconds for it to re-appear
+  await sleep(6000);
+  await pinInput.focus();
   console.log('entering pin');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.press('Tab');
-  await page.keyboard.type(CARNET_PIN);
+  await page.keyboard.type(CARNET_PIN, { delay: 100 });
   await page.keyboard.press('Enter');
   await page.waitForNavigation();
   console.log('going to trip page');
-  await page.goto('https://carnet.vw.com/group/vwcwp/trips');
-  const dataElement = await page.$(
-    '#_tripstatisticsportlet_WAR_tripstatisticsportlet_tripsJson'
-  );
+  await page.goto('https://carnet.vw.com/group/vwcwp1/trips');
+  const dataElement = await page.$('[name*=tripsJson]');
   console.log('found trip data');
   const tripData = JSON.parse(await (await dataElement.getProperty('value')).jsonValue());
   await browser.close();
